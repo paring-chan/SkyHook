1e3a724a1ad1f5d8c9270896857691ba64fef0ea
Pariring <paring@pikokr.dev>
2022-11-01 17:32:56 +0900

    feat: windows platform base
    Co-Authored-By CrackThrough <crackthrough@gmail.com>

--- skyhook/src/platforms/windows/hook.rs
+++ skyhook/src/platforms/windows/hook.rs
@@ -1,5 +1,9 @@
+// unsafe stuff
 extern crate user32;
 extern crate winapi;
+
+// safe stuff
+extern crate winsafe;
 
 //#region Constants
 const KEY_PRESSED: i32 = 0x8000;
@@ -8,22 +12,41 @@ const WM_KEYUP: i32 = 0x0101;
 const WM_SYSKEYDOWN: i32 = 0x0104;
 const WM_SYSKEYUP: i32 = 0x0105;
 const WH_KEYBOARD_LL: i32 = 13;
+const WH_MOUSE_LL: i32 = 14;
 //#endregion
 
-/*
-fn main() {
-    let hook_id = user32::SetWindowsHookExA(
-        WH_KEYBOARD_LL,
-        Some(hook_callback),
-        // No idea what goes here ,
-        0,
-    );
-}
-*/
+//#region Commons
+static mut hook_id: winsafe::HHOOK = 0;
 
-fn hook_callback(code: i32, wParam: u64, lParam: i64) -> i64 {
-    
+pub fn start() -> () {
+	unsafe {
+        let hook_id =
+            user32::SetWindowsHookExA(WH_KEYBOARD_LL, Some(hook_callback), std::ptr::null_mut(), 0);
+
+		// Do something
+
+        // Release hook
+        user32::UnhookWindowsHookEx(hook_id);
+    }
 }
+
+pub fn stop() -> () {
+	
+}
+//#endregion
+
+
+extern "system" fn hook_callback(code: i32, wParam: u64, lParam: i64) -> i64 {
+	unsafe {
+		assert!(hook_id != 0, format!("Windows threw an error with code {}",
+			winapi::um::errhandlingapi::GetLastError())); // GetLastError is an unsafe method
+
+		
+	}
+
+    return 0;
+}
+
 /**
         internal delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
