b717b0188717db908d71afd9c5d283c72c13f7a1
Pariring <paring@pikokr.dev>
2022-11-02 20:58:01 +0900

    feat: message loop

--- skyhook/src/platforms/windows/hook.rs
+++ skyhook/src/platforms/windows/hook.rs
@@ -1,11 +1,20 @@
 use std::{thread::Builder, time::SystemTime};
 
-use winsafe::{co::{WH, WM}, HHOOK, HINSTANCE};
+use winsafe::{
+    co::{WH, WM},
+    msg::wm,
+    prelude::kernel_Hthread,
+    prelude::{user_Hhook, Handle},
+    HHOOK, HINSTANCE,
+};
 
 use crate::types::{Error, Event, EventData};
 
+use super::{message::process_message, CANCELLATION_TOKEN};
+
 //#region Commons
 static mut HOOK_ID: Option<HHOOK> = None;
+static mut THREAD_ID: Option<u32> = None;
 static mut CALLBACK: Option<fn(Event)> = None;
 
 pub fn start(callback: fn(Event)) -> Result<(), Error> {
@@ -21,54 +30,94 @@ pub fn start(callback: fn(Event)) -> Result<(), Error> {
         CALLBACK = Some(callback);
     }
 
+    let cancellation_token = match unsafe { &CANCELLATION_TOKEN } {
+        Some(v) => v,
+        None => {
+            return Err(Error {
+                message: "Cancellation token is None.".into(),
+            })
+        }
+    };
+
     let thread = Builder::new().spawn(|| {
         let registered_hook = HHOOK::SetWindowsHookEx(
             WH::KEYBOARD_LL,
             hook_callback,
             Some(HINSTANCE::NULL),
-            Some(0));
+            Some(0),
+        );
 
         unsafe {
             HOOK_ID = match registered_hook {
                 Ok(h) => Some(h),
-                Err(err) => return Err(err)
+                Err(err) => return Err(err),
             };
+
+            let thread_id = winsafe::HTHREAD::GetCurrentThreadId();
+
+            THREAD_ID = Some(thread_id);
         }
 
-        return Ok(());
+        process_message(cancellation_token);
+
+        Ok(())
     });
 
-    match thread {
-        Ok(processed_thread) => {
-            match processed_thread.join() {
-                Ok(result) => {
-                    match result {
-                        Err(err) => Err(Error {message: format!("Could not start the hook. {:?}", err)}),
-                        _ => Ok(())
-                    }
-                },
-                Err(err) => {
-                    Err(Error {message: format!("A panic occured in the thread. {:?}", err)})
-                }
-            }
-        },
-        Err(err) => {
-            Err(Error {message: format!("Could not create a thread for the hook. {:?}", err)})
-        }
+    while let None = unsafe { HOOK_ID } {}
+
+    // match thread {
+    //     Ok(processed_thread) => match processed_thread.join() {
+    //         Ok(result) => match result {
+    //             Err(err) => Err(Error {
+    //                 message: format!("Could not start the hook. {:?}", err),
+    //             }),
+    //             _ => Ok(()),
+    //         },
+    //         Err(err) => Err(Error {
+    //             message: format!("A panic occured in the thread. {:?}", err),
+    //         }),
+    //     },
+    //     Err(err) => Err(Error {
+    //         message: format!("Could not create a thread for the hook. {:?}", err),
+    //     }),
+    // }
+
+    if let Err(e) = thread {
+        return Err(Error {
+            message: format!("Failed to start hook thread: {:?}", e),
+        });
     }
+
+    Ok(())
 }
 
 pub fn stop() -> Result<(), Error> {
     unsafe {
         if let Some(hook_id) = HOOK_ID {
             match HHOOK::UnhookWindowsHookEx(hook_id) {
-                Ok(_) => return Ok(()),
-                Err(err) => return Err(Error {message: format!("Could not stop the hook. {:?}", err)})
+                Ok(_) => (),
+                Err(err) => {
+                    return Err(Error {
+                        message: format!("Could not stop the hook. {:?}", err),
+                    })
+                }
             };
+        }
+
+        if let Some(thread_id) = THREAD_ID {
+            if let Err(e) = winsafe::PostThreadMessage(thread_id, wm::Close {}) {
+                return Err(Error {
+                    message: format!("Failed to send close message: {:?}", e),
+                });
+            }
+
+            return Ok(());
         }
     }
 
-    Err(Error {message: "Hook cannot be stopped before starting.".to_string()})
+    Err(Error {
+        message: "Hook cannot be stopped before starting.".into(),
+    })
 }
 //#endregion
 
@@ -76,35 +125,33 @@ pub fn stop() -> Result<(), Error> {
 extern "system" fn hook_callback(code: i32, w_param: usize, l_param: isize) -> isize {
     let processed_hook_id: HHOOK;
 
+    println!("Code: {} w_param: {} l_param: {}", code, w_param, l_param);
+
     unsafe {
         processed_hook_id = HOOK_ID.unwrap();
     }
 
     if code < 0 {
         // Don't do anything, just return
-        return processed_hook_id.CallNextHookEx(WH::from(code), w_param, l_param);
+        return processed_hook_id.CallNextHookEx(code.into(), w_param, l_param);
     }
 
-    match WM::from(w_param as u32) {
-        WM::KEYDOWN | WM::SYSKEYDOWN => {
-            unsafe {
-                CALLBACK.unwrap() (Event {
-                    time: SystemTime::now(),
-                    data: EventData::KeyPress(l_param as u16),
-                });
-            }
+    match (w_param as u32).into() {
+        WM::KEYDOWN | WM::SYSKEYDOWN => unsafe {
+            CALLBACK.unwrap()(Event {
+                time: SystemTime::now(),
+                data: EventData::KeyPress(l_param as u16),
+            });
         },
-        WM::KEYUP | WM::SYSKEYUP => {
-            unsafe {
-                CALLBACK.unwrap() (Event {
-                    time: SystemTime::now(),
-                    data: EventData::KeyRelease(l_param as u16),
-                });
-            }
+        WM::KEYUP | WM::SYSKEYUP => unsafe {
+            CALLBACK.unwrap()(Event {
+                time: SystemTime::now(),
+                data: EventData::KeyRelease(l_param as u16),
+            });
         },
-        _ => ()
+        _ => (),
     }
 
     // ALWAYS call CallNextHookEx
-    return processed_hook_id.CallNextHookEx(WH::from(code), w_param, l_param);
+    return processed_hook_id.CallNextHookEx(code.into(), w_param, l_param);
 }
--- skyhook/src/platforms/windows/mod.rs
+++ skyhook/src/platforms/windows/mod.rs
@@ -1,11 +1,33 @@
+use std::{thread, time::Duration};
+
+use cancellation::CancellationTokenSource;
+
 use crate::types::Event;
 
 mod hook;
+mod message;
+
+pub static mut CANCELLATION_TOKEN: Option<CancellationTokenSource> = None;
 
 pub fn start(callback: fn(Event)) -> Result<(), crate::types::Error> {
+    unsafe {
+        CANCELLATION_TOKEN = Some(CancellationTokenSource::new());
+    }
+
     hook::start(callback)
 }
 
 pub fn stop() -> Result<(), crate::types::Error> {
-    hook::stop()
+    hook::stop()?;
+    unsafe {
+        if let Some(token) = &CANCELLATION_TOKEN {
+            token.cancel();
+        }
+
+        CANCELLATION_TOKEN = None;
+    }
+
+    thread::sleep(Duration::from_secs(1));
+
+    Ok(())
 }
