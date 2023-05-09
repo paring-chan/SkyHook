6d68282a69c9ac4b7c461ea98421fc872f51b2e6
paring <paring@pikokr.dev>
2023-05-09 12:21:39 +0900

    feat: use hook method enum

---
+++ skyhook/src/platforms/linux/methods.rs
@@ -0,0 +1,4 @@
+pub enum LinuxHookMethod {
+    Unset,
+    InputDev,
+}
--- skyhook/src/platforms/linux/mod.rs
+++ skyhook/src/platforms/linux/mod.rs
@@ -1,12 +1,41 @@
 use crate::types::{Error, Event};
 
+use self::methods::LinuxHookMethod;
+
 mod inputdev;
+mod methods;
+
+static mut SELECTED_METHOD: LinuxHookMethod = LinuxHookMethod::Unset;
 
 pub fn start(callback: fn(Event)) -> Result<(), Error> {
-    inputdev::start(callback)
+    if let LinuxHookMethod::Unset = unsafe { &SELECTED_METHOD } {
+        inputdev::start(callback)?;
+        unsafe {
+            SELECTED_METHOD = LinuxHookMethod::InputDev;
+        }
+        Ok(())
+    } else {
+        Err(Error {
+            message: "Hook is already running".into(),
+        })
+    }
 }
 
 #[allow(dead_code)]
 pub fn stop() -> Result<(), Error> {
-    inputdev::stop()
+    match unsafe { &SELECTED_METHOD } {
+        LinuxHookMethod::InputDev => {
+            inputdev::stop()?;
+            unsafe {
+                SELECTED_METHOD = LinuxHookMethod::Unset;
+            }
+        }
+        _ => {
+            return Err(Error {
+                message: "The hook method is not set properly".into(),
+            })
+        }
+    };
+
+    Ok(())
 }
