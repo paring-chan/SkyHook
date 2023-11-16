8acaa689064d20972222e9b9da27c9ba9728dff2
paring <paring@pikokr.dev>
2023-11-16 22:22:04 +0900

    feat: mouse listener

--- skyhook/src/platforms/macos/mod.rs
+++ skyhook/src/platforms/macos/mod.rs
@@ -4,28 +4,27 @@ use std::ptr::null_mut;
 use std::thread;
 use std::thread::Builder;
 
-use crate::types::{Error, Event, EventData};
 use core_foundation::{
     base::kCFAllocatorDefault,
-    runloop::{kCFRunLoopDefaultMode, CFRunLoop},
+    runloop::{CFRunLoop, kCFRunLoopDefaultMode},
 };
 use core_foundation::base::ToVoid;
 use core_foundation::dictionary::{CFDictionaryCreateMutable, CFDictionarySetValue, kCFTypeDictionaryKeyCallBacks, kCFTypeDictionaryValueCallBacks};
 use core_foundation::number::{CFNumberCreate, kCFNumberIntType};
-use core_foundation::runloop::{CFRunLoopGetCurrent, CFRunLoopRun};
-
-use core_graphics::event::{CGEvent, CGEventTap, CGEventTapProxy, CGEventType};
+use core_foundation::runloop::CFRunLoopGetCurrent;
 use io_kit_sys::CFSTR;
-use io_kit_sys::hid::base::IOHIDValueRef;
-use io_kit_sys::hid::element::IOHIDElementGetUsage;
+use io_kit_sys::hid::base::{IOHIDValueCallback, IOHIDValueRef};
+use io_kit_sys::hid::element::{IOHIDElementGetUsage, IOHIDElementGetUsagePage};
 use io_kit_sys::hid::keys::{kIOHIDOptionsTypeNone, kIOHIDPrimaryUsageKey};
 use io_kit_sys::hid::manager::{IOHIDManagerCreate, IOHIDManagerOpen, IOHIDManagerRegisterInputValueCallback, IOHIDManagerScheduleWithRunLoop, IOHIDManagerSetDeviceMatching};
-use io_kit_sys::hid::usage_tables::kHIDUsage_GD_Keyboard;
+use io_kit_sys::hid::usage_tables::*;
 use io_kit_sys::hid::value::{IOHIDValueGetElement, IOHIDValueGetIntegerValue};
 use io_kit_sys::ret::{IOReturn, kIOReturnNotPermitted};
-use crate::debug;
+
 use crate::keycodes::VK;
+use crate::keycodes::VK::*;
 use crate::platforms::macos::keycode::raw_keycode_to_vk;
+use crate::types::{Error, Event, EventData};
 
 mod keycode;
 
@@ -91,6 +90,78 @@ unsafe extern "C" fn keyboard_callback(
     }
 }
 
+#[allow(non_upper_case_globals)]
+unsafe extern "C" fn mouse_callback(
+    _context: *mut c_void,
+    _result: IOReturn,
+    _sender: *mut c_void,
+    value: IOHIDValueRef,
+) {
+    let element = IOHIDValueGetElement(value);
+    // let device = IOHIDElementGetDevice(element);
+    let button_code = IOHIDElementGetUsage(element);
+    let element_value = IOHIDValueGetIntegerValue(value);
+
+    if button_code == u32::MAX {
+        return;
+    }
+
+    let vk = match button_code {
+        kHIDUsage_Button_1 => MouseLeft,
+        kHIDUsage_Button_2 => MouseRight,
+        kHIDUsage_Button_3 => MouseMiddle,
+        kHIDUsage_Button_4 => MouseX1,
+        0x05 => MouseX2,
+        _ => return
+    };
+
+    match element_value {
+        0 => up(vk, button_code as i64),
+        1 => down(vk, button_code as i64),
+        _ => return
+    }
+}
+
+unsafe fn watch(device_type: *const c_void, callback: IOHIDValueCallback) {
+    let hid_manager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+
+    let match_dict = CFDictionaryCreateMutable(
+        kCFAllocatorDefault,
+        2,
+        &kCFTypeDictionaryKeyCallBacks,
+        &kCFTypeDictionaryValueCallBacks,
+    );
+
+    CFDictionarySetValue(
+        match_dict,
+        CFSTR(kIOHIDPrimaryUsageKey).to_void(),
+        CFNumberCreate(
+            kCFAllocatorDefault,
+            kCFNumberIntType,
+            device_type,
+        ).to_void(),
+    );
+
+    IOHIDManagerSetDeviceMatching(
+        hid_manager,
+        match_dict,
+    );
+    IOHIDManagerScheduleWithRunLoop(
+        hid_manager,
+        CFRunLoopGetCurrent(),
+        kCFRunLoopDefaultMode,
+    );
+
+
+    let result = IOHIDManagerOpen(hid_manager, kIOHIDOptionsTypeNone);
+
+    if result == kIOReturnNotPermitted {
+        RESULT = Some(Err("NOT_PERMITTED".to_string()));
+        return;
+    }
+
+    IOHIDManagerRegisterInputValueCallback(hid_manager, callback, null_mut());
+}
 
 pub fn start(#[allow(unused_variables)] callback: fn(Event)) -> Result<(), Error> {
     unsafe {
@@ -102,46 +173,8 @@ pub fn start(#[allow(unused_variables)] callback: fn(Event)) -> Result<(), Error
                 return;
             }
 
-            let device_type: *const c_void = (&kHIDUsage_GD_Keyboard as *const u32) as *const c_void;
-
-            let hid_manager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
-
-            let match_dict = CFDictionaryCreateMutable(
-                kCFAllocatorDefault,
-                2,
-                &kCFTypeDictionaryKeyCallBacks,
-                &kCFTypeDictionaryValueCallBacks,
-            );
-
-            CFDictionarySetValue(
-                match_dict,
-                CFSTR(kIOHIDPrimaryUsageKey).to_void(),
-                CFNumberCreate(
-                    kCFAllocatorDefault,
-                    kCFNumberIntType,
-                    device_type,
-                ).to_void(),
-            );
-
-            IOHIDManagerSetDeviceMatching(
-                hid_manager,
-                match_dict,
-            );
-            IOHIDManagerScheduleWithRunLoop(
-                hid_manager,
-                CFRunLoopGetCurrent(),
-                kCFRunLoopDefaultMode,
-            );
-
-
-            let result = IOHIDManagerOpen(hid_manager, kIOHIDOptionsTypeNone);
-
-            if result == kIOReturnNotPermitted {
-                RESULT = Some(Err("NOT_PERMITTED".to_string()));
-                return;
-            }
-
-            IOHIDManagerRegisterInputValueCallback(hid_manager, keyboard_callback, null_mut());
+            watch((&kHIDUsage_GD_Keyboard as *const u32) as *const c_void, keyboard_callback);
+            watch((&kHIDUsage_GD_Mouse as *const u32) as *const c_void, mouse_callback);
 
             LOOP = Some(CFRunLoop::get_current());
 
